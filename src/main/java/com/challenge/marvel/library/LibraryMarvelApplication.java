66e4315196ebb347cb3204807b5df64d5403db48
Saul Sanchez S <saul.sanchez@globant.com>
2023-06-28 12:07:01 -0600

    Initial project

---
+++ src/main/java/com/challenge/marvel/library/LibraryMarvelApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.marvel.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMarvelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/marvel/library/LibraryMarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.marvel.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
